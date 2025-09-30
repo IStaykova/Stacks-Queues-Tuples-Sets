04ea5894aef3f8be07d084917f4cad023767d2ab
IStaykova <164316912+IStaykova@users.noreply.github.com>
2025-09-30 20:22:47 +0300

    Update expression_evaluator.py

--- expression_evaluator.py
+++ expression_evaluator.py
@@ -1,1 +1,22 @@
+from collections import deque
+
 expression = input().split()
+numbers = deque()
+
+operators = {
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "/": lambda a, b: a // b
+}
+
+for ch in expression:
+    if ch not in "+-*/":
+        numbers.append(int(ch))
+    else:
+        while len(numbers) > 1:
+            first_num = numbers.popleft()
+            second_num = numbers.popleft()
+            numbers.appendleft(operators[ch](first_num, second_num))
+
+print(numbers[0])
